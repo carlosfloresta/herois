604654e45691eb091312ea7ea462ca1d800d293b
carlosfloresta <carlos.floresta@etec.sp.gov.br>
2019-04-10 22:17:19 -0300

    projeto em andamento android herois

---
+++ app/src/main/java/br/com/etecia/appherois/HeroisActivity.java
@@ -0,0 +1,17 @@
+package br.com.etecia.appherois;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HeroisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.herois_activity);
+    }
+
+    public void abrirPersonagem(View view) {
+    }
+}
